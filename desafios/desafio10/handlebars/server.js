f72477a19c96431dfe1894ea953e14df05402232
Ezequiel Emanuel Ramirez <ezequielram@gmail.com>
2022-11-20 21:39:02 -0300

    handlebars desafio

---
+++ desafios/desafio10/handlebars/server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const handlebars = require('express-handlebars')
+const app = express()
+
+const productos = []
+let listExist = false
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+//with handlebars
+
+app.engine('handlebars', handlebars.engine())
+app.set('views', './views')
+app.set('view engine', 'handlebars')
+
+// get
+
+app.get('/', (req, res) => {
+    res.render('formulario')
+})
+
+app.get('/productos', (req, res) => {
+    if(productos.length > 0){
+        listExist = true
+    }
+    res.render('historial', { productos, listExist })
+})
+
+// post
+
+app.post('/productos', (req, res) => {
+    productos.push(req.body)
+    res.redirect('/productos')
+})
+
+app.listen(8080)
+
+
+
