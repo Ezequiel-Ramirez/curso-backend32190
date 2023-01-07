51ae38f723bccf7fa93d280f7c18e25a90c720ea
Ezequiel Emanuel Ramirez <ezequielram@gmail.com>
2023-01-07 17:51:03 -0300

    agrego productos por faker.js

--- desafios/desafio22/src/routes/index.routes.js
+++ desafios/desafio22/src/routes/index.routes.js
@@ -1,24 +1,46 @@
 import { Router } from "express";
 import Producto from "../models/Producto";
+import { faker } from '@faker-js/faker'
+faker.locale = 'en'
+
+function getProduct(id) {
+    return {
+        id: id,
+        title: faker.commerce.productName(),
+        price: faker.commerce.price(),
+        thumbnail: faker.image.abstract()
+    }
+}
+
+
+
 
 const router = Router();
 
 
-router.get("/", (req, res) => {
-    res.render('index')
+router.get("/", async (req, res) => {
+    const productos = await Producto.find().lean();
+    res.render('index', { productos })
 })
 
 router.post("/api/productos/guardar", async (req, res) => {
     const { title, price, thumbnail } = req.body;
     const newProducto = new Producto({ title, price, thumbnail });
     await newProducto.save();
-
-
-    res.send('guardar')
+    res.redirect('/')
 })
 
 router.get("/api/productos", (req, res) => {
-    res.render('productos')
+    const cant = Number(req.query.cant) || 1
+    const objs = []
+
+    for (let i = 0; i < cant; i++) {
+        objs.push(getProduct(i + 1))
+        const newProducto = new Producto({ title: objs[i].title, price: objs[i].price, thumbnail: objs[i].thumbnail });
+        newProducto.save();
+    }
+
+    res.render('productos', { objs })
 })
 
 export default router;
