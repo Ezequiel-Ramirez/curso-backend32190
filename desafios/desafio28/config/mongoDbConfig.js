a4af7ed8148b03a903a2837164a0881d87a3acb2
Ezequiel Emanuel Ramirez <ezequielram@gmail.com>
2023-01-28 20:49:10 -0300

    carga inicial desafio 28

---
+++ desafios/desafio28/config/mongoDbConfig.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const connect = async () => {
+    
+    try {
+        const URL = "mongodb+srv://ezequiel:ezequiel@backendcodercurso.y3plhcv.mongodb.net/desafio28?retryWrites=true&w=majority"
+        mongoose.connect(URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+
+        console.log("conectados correctamente")
+
+    } catch (error) {
+        console.log("error al conectarse a la base de datos", error)
+    }
+}
+
+module.exports = { connect };
