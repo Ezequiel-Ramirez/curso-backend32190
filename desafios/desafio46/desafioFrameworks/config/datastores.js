20ed08c69abeacff9a7f928a638ee05c8d96aae5
Ezequiel Emanuel Ramirez <ezequielram@gmail.com>
2023-04-07 18:42:01 -0300

    api productos

--- desafios/desafio46/desafioFrameworks/config/datastores.js
+++ desafios/desafio46/desafioFrameworks/config/datastores.js
@@ -48,8 +48,8 @@ module.exports.datastores = {
     *    (See https://sailsjs.com/config/datastores for help.)                 *
     *                                                                          *
     ***************************************************************************/
-    // adapter: 'sails-mysql',
-    // url: 'mysql://user:password@host:port/database',
+    adapter: 'sails-mongo',
+    url: 'mongodb+srv://ezequiel:ezequiel@backendcodercurso.y3plhcv.mongodb.net/desafio40B?retryWrites=true&w=majority',
 
   },
 
